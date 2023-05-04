e7be080fc04b0ebf0075c54cbe9e0bc8c15ff0a1
Jayachandra Reddy K <jpreddy369@gmail.com>
2023-05-04 11:55:20 +0530

    1st day Lab and hands on programs from 1 to 4

--- date_time_program_2.py
+++ date_time_program_2.py
@@ -1,7 +1,8 @@
 import datetime
 from datetime import *
 now = datetime.now()
-print(now)
+print('now command is bieng printed:', now)
+print('date is being printed:', date)
 
 print(f'Date now: {now.day}/{now.month}/{now.year}')
 print(f'Time now: {now.hour}:{now.minute}:{now.second}')
